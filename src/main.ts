229d3cd1ba140c9d0637f7cd070db29e112ce846
Yehyv <101909827+Yehyv@users.noreply.github.com>
2025-09-12 15:17:35 +0300

    adding local domain to handl cors errors

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   );
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new AllExceptionsFilter());
-  app.enableCors({ origin: 'http://72.60.39.170' });
+  app.enableCors({ origin: ['http://72.60.39.170', 'http://localhost:5173'] });
 
   await app.listen(process.env.PORT ?? 3000);
 }
