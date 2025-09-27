3d8ec07852715ac843a9d5db4e16f10938299ea2
AbdullahShabaan <131316365+AbdullahShabaan@users.noreply.github.com>
2025-09-27 14:21:10 +0300

    feat: add user dropdown with profile navigation

---
+++ .history/src/features/UserHome/components/InstituteCoursesSection_20250926151613.tsx
@@ -0,0 +1,74 @@
+import SectionTitle from "@/shared/components/SectionTitle";
+import Slider from "react-slick";
+import ArrowButton from "@/shared/components/ui/ArrowButton";
+import GhostButton from "@/shared/components/ui/GhostButton";
+import { useQuery } from "@tanstack/react-query";
+import CardSkeleton from "@/shared/components/ui/CardSkeleton";
+import SliderErrorFallback from "@/shared/utils/SliderErrorFallback";
+import { useLanguage } from "@/shared/localization/useLanguage";
+import { useResponsiveSlides } from "@/shared/utils/useResponsiveSlides";
+import InstituteCourseCard from "./InstituteCourseCard";
+import { getInstituteCoursesForSlider } from "../services/userHomeApis";
+const InstituteCoursesSection = () => {
+  const { t } = useLanguage();
+  const { data, isLoading, error } = useQuery<any[]>({
+    queryKey: ["InstituteCoursesSection"],
+    queryFn: getInstituteCoursesForSlider as any,
+  });
+
+  const { slidesToShow, windowWidth } = useResponsiveSlides(
+    [
+      { width: 600, slides: 1 },
+      { width: 1000, slides: 2 },
+      { width: 1180, slides: 3 },
+    ],
+    3 // default
+  );
+
+  console.log(data);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow,
+    slidesToScroll: 1,
+    accessibility: true,
+    nextArrow: windowWidth >= 1180 ? <ArrowButton direction="right" /> : <></>,
+    prevArrow: windowWidth >= 1180 ? <ArrowButton direction="left" /> : <></>,
+  };
+
+  if (error)
+    return (
+      <SliderErrorFallback componentTitle={t("institute_courses_title")} />
+    );
+
+  return (
+    <div className="mb-5 container">
+      <div className="flex justify-between items-start">
+        <SectionTitle textTitle={t("institute_courses_title")} />
+        <GhostButton buttonText={t("more")} to="/institute-courses" />
+      </div>
+
+      {isLoading ? (
+        <div className="grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mb-8">
+          {Array.from({ length: 3 }).map((_, i) => (
+            <CardSkeleton key={i} />
+          ))}
+        </div>
+      ) : (
+        <div className="w-full py-5">
+          <Slider {...settings}>
+            {data?.map((courseData, idx) => (
+              <div key={idx} className="px-2">
+                <InstituteCourseCard key={idx} course={courseData} />
+              </div>
+            ))}
+          </Slider>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default InstituteCoursesSection;
